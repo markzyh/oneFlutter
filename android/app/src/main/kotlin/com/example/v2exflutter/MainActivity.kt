e24f79f296c0adce7e1ba6c42ddc1c2a6471dfb1
Markzyh <980511383@qq.com>
2019-10-31 16:58:32 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/v2exflutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.v2exflutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
